b2413a4cd2e49eab6cec4850cae3d382b9a6d132
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-05-17 11:46:53 +0300

    add Class Graph

---
+++ src/graph.py
@@ -0,0 +1,38 @@
+class Graph:
+    """This class creates a graph object that has the name
+    of the graph, the number of nodes and the number of edges."""
+    def __init__(self, name, nodes, edges):
+        """This function initializes the graph object.
+
+        Args:
+            name (string): name of the graph
+            nodes (integer): number of nodes
+            edges (integer): number of edges
+        """
+        self.name = name
+        self.nodes = nodes
+        self.edges = edges
+
+    def get_names(self):
+        """This function returns the name of the graph.
+
+        Returns:
+            string: name
+        """
+        return self.names
+
+    def get_nodes(self):
+        """This function returns the number of nodes in the graph.
+
+        Returns:
+            integer: nodes
+        """
+        return self.nodes
+
+    def get_edges(self):
+        """This function returns the number of edges in the graph.
+
+        Returns:
+            integer: edges
+        """
+        return self.edges
