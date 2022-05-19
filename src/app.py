103b4f5cc92bf0dda17ea9027e5c0278f7ec1712
Petri <yogho@hotmail.com>
2022-05-19 13:00:57 +0300

    main thing to app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from os import getenv
+from os import getenv, environ
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +7,5 @@ app.secret_key = getenv("SECRET_KEY")
 import routes
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
