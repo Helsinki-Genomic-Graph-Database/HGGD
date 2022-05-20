4a9fc4db5b586e8d2dab35dc97e4f54bf5970899
Henrik Hautajärvi <henrik.hautajarvi@gmail.com>
2022-05-20 12:58:25 +0300

    turn off debug-mode

--- src/app.py
+++ src/app.py
@@ -58,4 +58,4 @@ def render_graph(name):
 
 if __name__ == "__main__":
     port = int(environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
