57b3d932b305bf724df3b9af2e99a801cb476ac5
Petri <yogho@hotmail.com>
2022-05-24 10:23:21 +0300

    route tests running again

---
+++ src/tests/test_routes.py
@@ -0,0 +1,19 @@
+
+import unittest
+from src.app import get_app
+
+class TestRoutes(unittest.TestCase):
+
+    def setUp(self):
+        self.app = get_app().test_client()
+        
+    def test_index(self):
+        with self.app as test_client:
+            res = test_client.get("/index")
+            assert res.status_code == 200
+        
+    def test_wrong_url_fails(self):
+        with self.app as test_client:
+            res = test_client.get("/xx")
+            assert res.status_code != 200
+
