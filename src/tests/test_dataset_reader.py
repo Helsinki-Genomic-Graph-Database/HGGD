1b73969e0f313743b343bea41a14765687c7646d
Petri <yogho@hotmail.com>
2022-05-25 15:08:40 +0300

    dataset_reader reads folders

---
+++ src/tests/test_dataset_reader.py
@@ -0,0 +1,16 @@
+import unittest
+from src.file_ui.dataset_reader import DatasetReader
+
+class TestDatasetReader(unittest.TestCase):
+
+    def setUp(self):
+        DIR = "src/tests/testdata_for_dataset_reader"
+        self.dataset_reader = DatasetReader(DIR)
+
+    def test_dataset_reader_should_return_list_of_three_paths(self):
+        res = self.dataset_reader.get_paths()
+        self.assertEqual(len(res), 2)
+
+    def test_list_should_include_path_to_test2_folder(self):
+        res = self.dataset_reader.get_paths()
+        assert "src/tests/testdata_for_dataset_reader/test2" in res
