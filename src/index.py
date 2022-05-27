4f7f042625ce33fb7941ec63a36daf1aaa946067
Petri <yogho@hotmail.com>
2022-05-27 12:30:16 +0300

    fixed or 'fixed' import paths

--- src/index.py
+++ src/index.py
@@ -1,9 +1,17 @@
-from src.text_ui.ui import UI
+import sys
+org_path = sys.path[0]
+modified_path = org_path[:-4]
+sys.path[0] = modified_path
+
+from src.file_ui.dataset_reader import DatasetReader
+from src.file_ui.folder_reader import FolderReader
 
 def main():
-    print("main pyörii")
-    ui_now = UI()
-    ui_now.start()
+
+    reader = DatasetReader("src/tests/testdata_for_dataset_reader")
+    
+    f_reader = FolderReader(reader.get_paths())
+    print(f_reader.get_folder_info())
 
 if __name__ == "__main__":
     main()
