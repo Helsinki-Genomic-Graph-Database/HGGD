f08cfa06dffb08429dd499cde080460b14310c2c
Henrik Hautajärvi <henrik.hautajarvi@gmail.com>
2022-05-27 09:40:02 +0300

    fix zipcreator path and update tests

--- src/tests/test_zip_creator.py
+++ src/tests/test_zip_creator.py
@@ -2,6 +2,7 @@ from imghdr import tests
 import unittest
 import os
 import shutil
+import zipfile
 from src.zip_creator import zipcreator_service
 
 class TestZipReader(unittest.TestCase):
@@ -10,20 +11,25 @@ class TestZipReader(unittest.TestCase):
 
     def test_create_zip_no_testfolder(self):
         self.delete_zip_folder(self.directory)
-        zipfile = zipcreator_service.create_zip("zip1", self.directory)
-        self.assertEqual("zip1.zip", zipfile)
+        testzipfile = zipcreator_service.create_zip("zip1", self.directory)
+        self.assertTrue(zipfile.is_zipfile(f"{self.directory}/zip/{testzipfile}"))
+        self.assertEqual("zip1.zip", testzipfile)
 
     def test_create_zip_testfolder_exists_no_zip_file(self):
+        self.delete_zip_folder(self.directory)
         self.make_zip_folder(self.directory)
-        zipfile = zipcreator_service.create_zip("zip2", self.directory)
-        self.assertEqual("zip2.zip", zipfile)
+        testzipfile = zipcreator_service.create_zip("zip2", self.directory)
+        self.assertTrue(zipfile.is_zipfile(f"{self.directory}/zip/{testzipfile}"))        
+        self.assertEqual("zip2.zip", testzipfile)
 
     def test_create_zip_testfolder_exists_zip_file_exists(self):
         self.delete_zip_folder(self.directory)
         self.make_zip_folder(self.directory)
         self.make_zip_file("zip3", self.directory)
-        zipfile = zipcreator_service.create_zip("zip4", self.directory)
-        self.assertEqual("zip3.zip", zipfile)
+        testzipfile = zipcreator_service.create_zip("zip4", self.directory)
+        self.assertTrue(zipfile.is_zipfile(f"{self.directory}/zip/{testzipfile}"))
+        self.assertEqual("zip3.zip", testzipfile)
+        self.delete_zip_folder(self.directory) # deletes test-zipfolder after all tests
 
     def delete_zip_folder(self, directory):
         if os.path.exists(f"{directory}/zip"):
