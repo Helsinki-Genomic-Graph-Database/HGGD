7437dfd855047441323503f4ace18559af8f94e9
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-05-31 09:15:38 +0300

    add licence

--- src/tests/test_description_reader_in_app.py
+++ src/tests/test_description_reader_in_app.py
@@ -4,7 +4,8 @@ from src.file_ui.file_utils import read_description
 class TestDescriptionReader(unittest.TestCase):
 
     def test_reader(self):
-        name, descr_short, descr_long = read_description("src/tests/testdata_with_full_description")
+        name, descr_short, descr_long, licence = read_description("src/tests/testdata_with_full_description")
         self.assertEqual(name, "testdata with description")
         self.assertEqual(descr_long, "testing data")
         self.assertEqual(descr_short, "with full description")
+        self.assertEqual(licence, "test_licence")
