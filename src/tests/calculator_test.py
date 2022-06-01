85471156ddd281ee516a52c522ec405559804c56
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-06-01 11:34:48 +0300

    fix test setup: add path for dataset

--- src/tests/calculator_test.py
+++ src/tests/calculator_test.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
         self.graph_list = [Graph("name1", 11, 12), Graph("name2", 13, 14), Graph("name3", 15, 16)]
-        self.dataset = Dataset("name", self.graph_list)
+        self.dataset = Dataset("datasetpath", "name", self.graph_list)
 
     def test_get_no_nodes_and_edges(self):
         nodes, edges = self.calculator.get_no_nodes_and_edges(self.dataset)
