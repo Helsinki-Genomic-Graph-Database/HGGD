9cfe1ecc532ee17ffd921750785d5651a39abfe6
Petri <yogho@hotmail.com>
2022-06-01 14:55:32 +0300

    started log time control

--- src/tests/test_ui_run_time_checking.py
+++ src/tests/test_ui_run_time_checking.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from src.file_ui.folder_reader import FolderReader
 from src.text_ui.ui import UI
 from src.tests.stub_io import StubIO
+from src.file_ui.file_utils import check_log_update_after_file_modified
 
 class TestLogCreationWithNoLogInFolder(unittest.TestCase):
 
@@ -14,6 +15,8 @@ class TestLogCreationWithNoLogInFolder(unittest.TestCase):
     def tearDown(self):
         if os.path.exists(f"{self.DIR}/log.txt"):
             os.remove(f"{self.DIR}/log.txt")
+        if os.path.exists(f"{self.DIR}/test.graph"):
+            os.remove(f"{self.DIR}/test.graph")
 
     def test_should_notice_no_log(self):
         info_list = self.folder_reader.get_folder_info()
@@ -35,3 +38,15 @@ class TestLogCreationWithNoLogInFolder(unittest.TestCase):
         logstamp = datetime.now().isoformat(" ", "seconds")
         self.assertEqual(line, f"ui run on: {logstamp}")
 
+    def test_should_notice_file_modified_after_log_update(self):
+        inputs = ["test_name"]
+        io = StubIO(inputs)
+        ui = UI(self.folder_reader, io)
+        ui.start()
+        with open(f"{self.DIR}/test.graph", "w") as f:
+            f.write("test")
+        with open(f"{self.DIR}/log.txt") as log:
+            line = log.readline()
+        res = check_log_update_after_file_modified(f"{self.DIR}/test.graph", line)
+        self.assertEqual(res, False)
+        
