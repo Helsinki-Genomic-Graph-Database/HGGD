a2ccf43d847465de2c66176b78c9dc51b94ed739
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-06-07 10:39:14 +0300

    docstring

--- src/data_check/json_writer.py
+++ src/data_check/json_writer.py
@@ -1,11 +1,20 @@
 import json
 
 class JsonWriter:
-
+    """This class includes methods for writing json-files."""
     def __init__(self):
         pass
 
     def create_json_file(self, dataset, name, sh_desc, long_desc, licence):
+        """This method created a new json files or writes to an empty one.
+
+        Args:
+            dataset
+            name (string)
+            sh_desc (string): short description
+            long_desc (string): long description
+            licence (string)
+        """
         dataset_dict = {
             "name" : name,
             "descr_short" : sh_desc,
@@ -16,6 +25,13 @@ class JsonWriter:
             json.dump(dataset_dict, file)
 
     def update_json_file(self, dataset, key, value):
+        """This method add information to an existing json file.
+
+        Args:
+            dataset
+            key (string)
+            value (string)
+        """
         json_path = self.get_json_path(dataset)
         new_info = {key : value}
         with open(json_path, "r+", encoding='utf-8') as file:
