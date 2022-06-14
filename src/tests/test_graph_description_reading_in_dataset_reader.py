d650b8862597bb2854a1aebb06a88bfcb63e1ebb
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-06-14 15:35:34 +0300

    fix test_dataset_should_have_list_of_tuples_for_graphs

--- src/tests/test_graph_description_reading_in_dataset_reader.py
+++ src/tests/test_graph_description_reading_in_dataset_reader.py
@@ -12,7 +12,7 @@ class TestGraphDescriptionReadingToDataset(unittest.TestCase):
 
     def test_dataset_should_have_list_of_tuples_for_graphs(self):
         print(self.res)
-        self.assertEqual(len(self.res), 10)
+        self.assertEqual(len(self.res), 9)
 
     def test_info_list_should_have_true_for_gfa_for_licence_in_description(self):
         for graph, has_licence, has_sources in self.res:
