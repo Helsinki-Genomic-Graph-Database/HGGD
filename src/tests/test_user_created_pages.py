9afef2acbb640f2fdead1387d2b77e03981e76ef
Petri <yogho@hotmail.com>
2022-06-17 13:53:13 +0300

    added user defined webpage creation

--- src/tests/test_user_created_pages.py
+++ src/tests/test_user_created_pages.py
@@ -2,22 +2,40 @@ import unittest
 import os
 import json
 
+
 from src.website_creator.user_defined_page_creator import UserDefinedPageCreator
+from src.app import get_app
 
 class TestPageCreator(unittest.TestCase):
 
-    def setUp(self):
-        self.folder = "src/tests/mock_templatefolder"
-        self.about = json.dumps({"name": "about", "content": "about tests"})
-        self.howto = json.dumps({"name": "how to", "content": "like this"})
-
-    def test_page_creator_should_make_a_list_of_pages_from_json_files_in_templatefolder(self):
-
+    def write_about(self):
         with open(self.folder+"/about.json", "w") as file:
             file.write(self.about)
 
+    def write_howto(self):
         with open(self.folder+"/howto.json", "w") as file:
             file.write(self.howto)
+
+    def remove_file(self, path):
+        if os.path.exists(path):
+            os.remove(path)
+
+    def setUp(self):
+        self.folder = os.getenv("USER_DEFINED_TEMPLATE_FOLDER")
+        self.about = json.dumps({"name": "About", "content": "about tests"})
+        self.howto = json.dumps({"name": "How to", "content": "like this"})
+
+    def tearDown(self):
+        self.remove_file(self.folder+"/about.json")
+        self.remove_file(self.folder+"/howto.json")
+        self.remove_file(self.folder+"/pages/about.html")
+        self.remove_file(self.folder+"/pages/howto.html")
+
+    def test_page_creator_should_make_a_dictionary_of_pages_from_json_files_in_templatefolder(self):
+
+        self.write_about()
+
+        self.write_howto()
 
         pcreator = UserDefinedPageCreator(self.folder)
 
@@ -25,4 +43,32 @@ class TestPageCreator(unittest.TestCase):
 
         self.assertEqual(len(res), 2)
 
-    # def test_page_creator_list_should_have
+    def test_page_creator_list_should_give_dictionary_with_html_filename_as_key(self):
+        self.write_about()
+        pcreator = UserDefinedPageCreator(self.folder)
+        res = pcreator.get_pages()
+        print(res)
+        self.assertEqual(res["about"], "About")
+        
+
+    def test_page_creator_should_create_html_file(self):
+        self.write_about()
+        pcreator = UserDefinedPageCreator(self.folder)
+        res = os.path.exists(self.folder+"/pages/about.html")
+        self.assertEqual(res, True)
+
+    def test_created_page_should_exist_on_webpage(self):
+        app = get_app().test_client()
+        self.write_about()
+        pcreator = UserDefinedPageCreator(self.folder)
+        res = app.get("hggd/pages/about")
+        self.assertEqual(res.status_code, 200)
+
+    def test_created_page_has_content_text(self):
+        app = get_app().test_client()
+        self.write_about()
+        pcreator = UserDefinedPageCreator(self.folder)
+        res = app.get("hggd/pages/about").data
+        self.assertIn(b"about tests", res)
+
+
--- src/website_creator/user_defined_page_creator.py
+++ src/website_creator/user_defined_page_creator.py
@@ -1,16 +1,33 @@
 import os
-from src.file_ui.file_utils import check_file_extension
+import json
+from src.file_ui.file_utils import check_file_extension, check_field, remove_file_extension
 
 class UserDefinedPageCreator:
 
     def __init__(self, user_defined_page_dir):
         self.user_defined_page_dir = user_defined_page_dir
         self.files = os.listdir(self.user_defined_page_dir)
-        self.created_pages = []
+        self.created_pages = {}
+        self.run()
 
-    def get_pages(self):
+    def run(self):
         for file in self.files:
             if check_file_extension(file, "json"):
-                self.created_pages.append(file)
+                with open(self.user_defined_page_dir+"/"+file) as jsonfile:
+                    content = json.load(jsonfile)
+                    name = check_field(content, "name")
+                    page_content = check_field(content, "content")
+                    file_wo_extension = remove_file_extension(file, "json")[:-1]
+                    path = self.user_defined_page_dir+"/pages/"+file_wo_extension+".html"
+                self.write_html(path, page_content)
+                file_wo_extension = remove_file_extension(file, "json")[:-1]
+                self.created_pages[file_wo_extension] = name
+                
+                
 
+    def get_pages(self):
         return self.created_pages
+
+    def write_html(self, url, page_content):
+        with open(url, "w") as html:
+                html.write(page_content)
